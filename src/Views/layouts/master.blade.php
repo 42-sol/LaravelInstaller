616bead937e0db8c9278f74d29f3aa3e6ba9fd3f
disaipe <disaipe@gmail.com>
2023-01-13 09:53:51 +0000

    Fix alert JS exception

--- src/Views/layouts/master.blade.php
+++ src/Views/layouts/master.blade.php
@@ -168,9 +168,12 @@
         <script type="text/javascript">
             var x = document.getElementById('error_alert');
             var y = document.getElementById('close_alert');
-            y.onclick = function() {
-                x.style.display = "none";
-            };
+
+            if (x && y) {
+                y.onclick = function() {
+                    x.style.display = "none";
+                };
+            }
         </script>
     </body>
 </html>
